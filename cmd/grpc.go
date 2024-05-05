c35b7b43b6f279f398b5398b5d64493c3d4c96ff
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-05-05 17:28:14 +0800

    configure: grpc

---
+++ cmd/grpc.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+)
+
+func StartGrpcServer(listener net.Listener) {
+	grpcServer := grpc.NewServer()
+
+	//services.AuthService(grpcServer)
+
+	// Запускаем gRPC сервер в отдельной горутине
+	go func() {
+		if err := grpcServer.Serve(listener); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	grpcServer.GracefulStop()
+}
