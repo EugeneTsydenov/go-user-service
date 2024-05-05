db9b73f7aec4ece42addd076b5b9eb303f0f38e7
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-05-05 20:13:29 +0800

    imlement: register logic

--- internal/repository/user.repository.go
+++ internal/repository/user.repository.go
@@ -1,1 +1,28 @@
 package repository
+
+import (
+	"github.com/EugeneTsydenov/go-user-service/cmd/user-service/db"
+	"github.com/EugeneTsydenov/go-user-service/internal/model"
+)
+
+func GetUserById(id int64) {
+
+}
+
+func GetUserByUsername(username string) (model.User, error) {
+	user := model.User{}
+	result := db.DB.First(&user, "username = ?", username)
+	return user, result.Error
+}
+
+func SaveUser(username, hashPassword string) error {
+	newUser := model.NewUser{
+		Username: username,
+		Password: hashPassword,
+	}
+	tx := db.DB.Table("users").Create(&newUser)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
+}
--- internal/service/user.service.go
+++ internal/service/user.service.go
@@ -1,9 +1,22 @@
 package service
 
 import (
+	"context"
 	"github.com/EugeneTsydenov/go-user-service/internal/proto"
+	"github.com/EugeneTsydenov/go-user-service/internal/repository"
+	"golang.org/x/crypto/bcrypt"
 	"google.golang.org/grpc"
 )
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
 
 type server struct {
 	proto.UnimplementedUserServiceServer
@@ -11,4 +24,20 @@ type server struct {
 
 func UserService(s *grpc.Server) {
 	proto.RegisterUserServiceServer(s, &server{})
+}
+
+func (s *server) Register(ctx context.Context, in *proto.RegisterRequest) (*proto.RegisterResponse, error) {
+	_, err := repository.GetUserByUsername(in.Username)
+	if err == nil {
+		return &proto.RegisterResponse{Success: false, Message: "User with this username is already exist!"}, nil
+	}
+	hashPassword, err := HashPassword(in.Password)
+	if err != nil {
+		return &proto.RegisterResponse{Success: false, Message: "Something error"}, nil
+	}
+	err = repository.SaveUser(in.Username, hashPassword)
+	if err != nil {
+		return &proto.RegisterResponse{Success: false, Message: "Something Error"}, nil
+	}
+	return &proto.RegisterResponse{Success: true, Message: "User successfully saved"}, nil
 }
